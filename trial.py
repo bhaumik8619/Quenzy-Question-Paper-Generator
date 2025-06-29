78d5296e7a4b218e2fac5cc901f945071033941b
Bhaumik Vaishnani <vaishnanibhaumik26@gmail.com>
2025-06-29 18:04:24 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -2,9 +2,6 @@ import os
 import google.generativeai as genai
 from PyPDF2 import PdfReader
 
-# Replace with your actual API key
-os.environ["GOOGLE_API_KEY"] = "AIzaSyCMqnIUe8vHMU2TShOeduh9aFJpSJ1cr14" 
-
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"]) 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
